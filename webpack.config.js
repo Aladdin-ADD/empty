9d4088d3c041c91289e471d1e6fd9282dc6d9383
薛定谔的猫 <zhaoshdm@yonyou.com>
2017-08-16 19:31:31 +0800

    Chore: module.exports.

--- webpack.config.js
+++ webpack.config.js
@@ -42,10 +42,4 @@ const developmentConfig = merge([
   parts.loadCSS()
 ])
 
-module.exports = (env) => {
-  if (env === 'production') {
-    return merge(commonConfig, productionConfig)
-  }
-
-  return merge(commonConfig, developmentConfig)
-}
+module.exports = (env) => merge(commonConfig, env === 'production' ? productionConfig : developmentConfig)
