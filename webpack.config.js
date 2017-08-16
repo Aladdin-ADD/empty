7225818cc074f8410488376e66cd575af9c971c7
薛定谔的猫 <zhaoshdm@yonyou.com>
2017-08-16 16:23:47 +0800

    Composing Configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,96 +1,47 @@
-const path = require("path");
-const webpack = require("webpack");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const merge = require('webpack-merge')
+
+const parts = require('./webpack.parts')
 
 const PATHS = {
-    app: path.join(__dirname, "app"),
-    build: path.join(__dirname, "build")
-};
+  app: path.join(__dirname, 'app'),
+  build: path.join(__dirname, 'build')
+}
 
-const commonConfig = {
-
-    // Entries have to resolve to files! They rely on Node
-    // convention by default so if a directory contains *index.js*,
-    // it resolves to that.
+const commonConfig = merge([
+  {
     entry: {
-        app: PATHS.app
+      app: PATHS.app
     },
     output: {
-        path: PATHS.build,
-        filename: "[name].js"
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                enforce: "pre",
-                loader: "eslint-loader",
-                options: { emitWarning: true }
-            }
-        ]
+      path: PATHS.build,
+      filename: '[name].js'
     },
     plugins: [
-        new HtmlWebpackPlugin({
-            title: "Webpack demo"
-        }),
-        new webpack.LoaderOptionsPlugin({
-            options: {
-                eslint: {
+      new HtmlWebpackPlugin({
+        title: 'Webpack demo'
+      })
+    ]
+  },
+  parts.lintJavaScript({ include: PATHS.app })
+])
 
-                    // Fail only on errors
-                    failOnWarning: false,
-                    failOnError: true,
+const productionConfig = merge([
+])
 
-                    // Toggle autofix
-                    fix: false,
+const developmentConfig = merge([
+  parts.devServer({
+    // Customize host/port here if needed
+    host: process.env.HOST,
+    port: process.env.PORT
+  })
+])
 
-                    // Output to Jenkins compatible XML
-                    outputReport: {
-                        filePath: "checkstyle.xml",
-                        formatter: require("eslint/lib/formatters/checkstyle")
-                    }
-                }
-            }
-        })
-    ],
-    bail: true
-};
+module.exports = (env) => {
+  if (env === 'production') {
+    return merge(commonConfig, productionConfig)
+  }
 
-function productionConfig() {
-    return commonConfig;
+  return merge(commonConfig, developmentConfig)
 }
-function developmentConfig() {
-    const config = {
-        devServer: {
-
-            // Enable history API fallback so HTML5 History API based
-            // routing works. Good for complex setups.
-            historyApiFallback: true,
-
-            // Display only errors to reduce the amount of output.
-            stats: "errors-only",
-
-            // Parse host and port from env to allow customization.
-            //
-            // If you use Docker, Vagrant or Cloud9, set
-            // host: options.host || '0.0.0.0';
-            //
-            // 0.0.0.0 is available to all network devices
-            // unlike default `localhost`.
-            host: process.env.HOST, // Defaults to `localhost`
-            port: process.env.PORT // Defaults to 8080
-        }
-    };
-
-    return Object.assign(
-        {},
-        commonConfig,
-        config
-    );
-}
-
-
-module.exports = env => {
-    console.log(env);
-    return env === "production" ? productionConfig() : developmentConfig();
-};
