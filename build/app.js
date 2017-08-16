8ceccec803496a6ab7f56fee220e56288670f8eb
薛定谔的猫 <zhaoshdm@yonyou.com>
2017-08-16 16:24:03 +0800

    Fix: eslint => standard.

--- build/app.js
+++ build/app.js
@@ -72,7 +72,7 @@ Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__component__ = __webpack_require__(1);
 
 
-document.body.appendChild(Object(__WEBPACK_IMPORTED_MODULE_0__component__["a" /* default */])());
+document.body.appendChild(Object(__WEBPACK_IMPORTED_MODULE_0__component__["a" /* default */])())
 
 
 /***/ }),
@@ -80,12 +80,12 @@ document.body.appendChild(Object(__WEBPACK_IMPORTED_MODULE_0__component__["a" /*
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
-/* harmony default export */ __webpack_exports__["a"] = ((text = "Hello world") => {
-    const element = document.createElement("div");
+/* harmony default export */ __webpack_exports__["a"] = ((text = 'Hello world') => {
+  const element = document.createElement('div')
 
-    element.innerHTML = text;
+  element.innerHTML = text
 
-    return element;
+  return element
 });
 
 
