95d68cff74c89f6957b1a2049a58054d3a48fd43
薛定谔的猫 <zhaoshdm@yonyou.com>
2017-08-18 12:16:10 +0800

    Code Splitting

--- app/component.js
+++ app/component.js
@@ -1,8 +1,16 @@
-export default (text = 'Hello world') => {
+export default () => {
   const element = document.createElement('div')
 
-  element.innerHTML = text
-  element.className = 'pure-button'
+  element.className = 'fa fa-hand-spock-o fa-1g'
+  element.innerHTML = 'Hello world'
+
+  element.onclick = () => {
+    import('./lazy').then((lazy) => {
+      element.textContent = lazy.default
+    }).catch((err) => {
+      console.error(err)
+    })
+  }
 
   return element
 }
